e33938146459a7eb97e98061841fde6c59a64207
Christopher Grim <christopher.grim@gmail.com>
2008-12-08 01:14:36 -0500

    add Task.find
    Supports basic searching and retrieving of Hiveminder tasks.

---
+++ lib/hiveminder.rb
@@ -0,0 +1,59 @@
+require 'activeresource'
+require 'cgi'
+
+module Hiveminder
+  # Set the SID used to login to Hiveminder.
+  def self.sid=(sid)
+    Base.sid = sid
+  end
+
+  # Set the Logger instance used for logging.
+  def self.logger=(logger)
+    Base.logger = logger
+  end
+
+  class Base < ActiveResource::Base #:nodoc:
+    self.site = "http://hiveminder.com"
+    @@cookie = nil
+
+    def self.sid= (sid)
+      @@cookie = CGI::Cookie.new('JIFTY_SID_HIVEMINDER', sid).to_s
+    end
+
+    def self.headers
+      headers = super
+      headers.update("Cookie" =>  @@cookie) if @@cookie
+      headers
+    end
+
+    def self.query_string(options)
+      "/#{options.collect.join('/')}" unless options.nil? || options.empty?
+    end
+
+    def self.element_path(id, prefix_options = {}, query_options = nil)
+      if query_options.nil?
+        prefix_options, query_options = split_options(prefix_options)
+      end
+      "/=/model#{prefix(prefix_options)}#{collection_name}/id/#{id}#{query_string(query_options)}.#{format.extension}"
+    end
+
+    # Searching for resources using the __order_by psuedo-column is
+    # the best way I could come up with to retrieve all of the
+    # resources in a collection using a GET.
+    #
+    # See <http://hiveminder.com/=/help/search>.
+    def self.collection_path(prefix_options = {}, query_options = nil)
+      prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+      "/=/search#{prefix(prefix_options)}#{collection_name}/__order_by/id#{query_string(query_options)}.#{format.extension}"
+    end
+
+    def self.instantiate_collection(collection, prefix_options = {})
+      collection = [ collection ] unless collection.kind_of? Array
+      super(collection.collect { |data| data["value"] }, prefix_options)
+    end
+  end
+
+  class Task < Base
+    self.collection_name = 'Task'
+  end
+end
---
+++ test/test_hiveminder.rb
@@ -0,0 +1,48 @@
+require 'test/unit'
+require 'hiveminder'
+
+class BaseTest < Test::Unit::TestCase
+  class Foo < Hiveminder::Base
+    self.collection_name = "Foo"
+  end
+
+  def test_element_path
+    assert_equal '/=/model/Foo/id/1.xml', Foo.element_path(1)
+  end
+
+  def test_collection_path
+    assert_equal "/=/search/Foo/__order_by/id.xml", Foo.collection_path
+  end
+
+  def test_collection_path_with_query_options
+    query = { :complete => 0 }
+    assert_equal "/=/search/Foo/__order_by/id/complete/0.xml", Foo.collection_path({ }, query)
+  end
+
+  def test_query_string
+    assert_equal "/foo/bar/flim/flam", Foo.query_string({ :foo => :bar, :flim => :flam })
+  end
+
+  def test_instantiate_collection
+    collection = %w(ping pong).collect { |e| { "value" => { "foo" => e }} }
+
+    resources = Foo.instantiate_collection(collection)
+    assert_equal 2, resources.length
+    resources.each_with_index do |r, i|
+      assert_kind_of Foo, r
+      assert_respond_to r, :foo
+      assert_equal collection[i]["value"]["foo"], r.foo
+    end
+  end
+
+  def test_instantiate_collection_should_handle_single_values
+    collection = { "value" => { "foo" => "bar" }}
+    resources = Foo.instantiate_collection(collection)
+    assert_equal 1, resources.length
+    r = resources.first
+    assert_kind_of Foo, r
+    assert_respond_to r, :foo
+    assert_equal "bar", r.foo
+  end
+end
+
